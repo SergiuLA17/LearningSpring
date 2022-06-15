400ec6aa061dce9778b7dd726b89440afd9bc108
SergiuA17 <98698995+SergiuLA17@users.noreply.github.com>
2022-06-15 19:32:44 +0300

    did tasks using the list

--- src/main/java/com/example/learningspring/StartPlayer.java
+++ src/main/java/com/example/learningspring/StartPlayer.java
@@ -6,8 +6,9 @@ public class StartPlayer {
     public static void main(String[] args) {
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml")) {
             PlayerMusic playerMusic = context.getBean("musicPlayer", PlayerMusic.class);
+;
             playerMusic.playMusic();
-            System.out.println(playerMusic.getName() + "\n" + playerMusic.getVolume());
+
 
         }
     }
